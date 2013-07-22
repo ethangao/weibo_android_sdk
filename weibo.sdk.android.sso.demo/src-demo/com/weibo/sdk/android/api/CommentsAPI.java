4dc677e7fb320ecac5166186d09a60c19c257fe0
gaochengxing <gaochengxing@xiaomi.com>
2013-07-22 10:37:08 +0800

    change the bug in CommentsAPI class

--- weibo.sdk.android.sso.demo/src-demo/com/weibo/sdk/android/api/CommentsAPI.java
+++ weibo.sdk.android.sso.demo/src-demo/com/weibo/sdk/android/api/CommentsAPI.java
@@ -59,7 +59,7 @@ public class CommentsAPI extends WeiboAPI {
 
 	/**
 	 * 获取当前登录用户所接收到的评论列表
-	 * 
+	 *
 	 * @param since_id 若指定此参数，则返回ID比since_id大的评论（即比since_id时间晚的评论），默认为0。
 	 * @param max_id 若指定此参数，则返回ID小于或等于max_id的评论，默认为0。
 	 * @param count 单页返回的记录条数，默认为50。
@@ -82,7 +82,7 @@ public class CommentsAPI extends WeiboAPI {
 
 	/**
 	 * 获取当前登录用户的最新评论包括接收到的与发出的
-	 * 
+	 *
 	 * @param since_id 若指定此参数，则返回ID比since_id大的评论（即比since_id时间晚的评论），默认为0。
 	 * @param max_id 若指定此参数，则返回ID小于或等于max_id的评论，默认为0。
 	 * @param count 单页返回的记录条数，默认为50。
@@ -130,7 +130,7 @@ public class CommentsAPI extends WeiboAPI {
 
 	/**
 	 * 根据评论ID批量返回评论信息
-	 * 
+	 *
 	 * @param cids 需要查询的批量评论ID数组，最大50。
 	 * @param listener
 	 */
@@ -147,7 +147,7 @@ public class CommentsAPI extends WeiboAPI {
 
 	/**
 	 * 对一条微博进行评论
-	 * 
+	 *
 	 * @param comment 评论内容，内容不超过140个汉字。
 	 * @param id 需要评论的微博ID。
 	 * @param comment_ori 当评论转发微博时，是否评论给原微博
@@ -158,16 +158,16 @@ public class CommentsAPI extends WeiboAPI {
 		params.add("comment", comment);
 		params.add("id", id);
 		if (comment_ori) {
-			params.add("comment_ori", 0);
-		} else {
 			params.add("comment_ori", 1);
+		} else {
+			params.add("comment_ori", 0);
 		}
 		request(SERVER_URL_PRIX + "/create.json", params, HTTPMETHOD_POST, listener);
 	}
 
 	/**
 	 * 删除一条评论
-	 * 
+	 *
 	 * @param cid 要删除的评论ID，只能删除登录用户自己发布的评论。
 	 * @param listener
 	 */
@@ -179,7 +179,7 @@ public class CommentsAPI extends WeiboAPI {
 
 	/**
 	 * 根据评论ID批量删除评论
-	 * 
+	 *
 	 * @param ids 需要删除的评论ID数组，最多20个。
 	 * @param listener
 	 */
@@ -196,7 +196,7 @@ public class CommentsAPI extends WeiboAPI {
 
 	/**
 	 * 回复一条评论
-	 * 
+	 *
 	 * @param cid 需要回复的评论ID。
 	 * @param id 需要评论的微博ID。
 	 * @param comment 回复评论内容，内容不超过140个汉字。
